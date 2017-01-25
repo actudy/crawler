17536e63ef7c8d71cc0c3dbe27a42b8c30b05dd6
Sanghoon LEE <sanghoon.lee@sotm-audio.com>
2017-01-25 10:02:19 +0900

    fix 404 error

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var results = require('./routes/results');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', index);
+app.get('/results', results);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
